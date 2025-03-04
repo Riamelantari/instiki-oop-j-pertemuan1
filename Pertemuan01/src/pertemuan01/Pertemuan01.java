4ae390803c8be2030c013420fd1c337c6b3af8b3
Riamelantari <riamelantari@gmail.com>
2025-03-04 21:38:51 +0800

    Pertemuan 1

---
+++ Pertemuan01/src/pertemuan01/Pertemuan01.java
@@ -0,0 +1,17 @@
+
+package pertemuan01;
+
+/**
+ *
+ * @author Komang Ria Melantari
+ */
+public class Pertemuan01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Belajar Bahasa Java");
+    }
+    
+}
